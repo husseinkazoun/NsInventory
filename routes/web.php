2cf6e732483cd472254191256cbb687603514d5b
husseinkazoun <husseinkazoun@users.noreply.github.com>
2025-08-02 13:26:11 +0000

    Initial project commit

--- routes/web.php
+++ routes/web.php
@@ -36,7 +36,7 @@ Route::get('php/', function () {
 });
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('dashboard');
 });
 
 Route::middleware(['auth'])->group(function () {
